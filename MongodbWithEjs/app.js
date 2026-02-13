63c5bdd0eb1255c3c5c8ebbab36862e73739300c
Shreya Sharma <sharmashreyaa21@gmail.com>
2026-02-13 20:07:26 +0530

    new

--- MongodbWithEjs/app.js
+++ MongodbWithEjs/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const userModel = require('./models/user');
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -14,6 +15,16 @@ app.get('/',(req,res) =>{
 
 app.get('/read',(req,res) =>{
     res.render("read");
+})
+
+app.post('/create', async (req,res) =>{
+    let{name,email,image}= req.body;
+   let createdUser =  await userModel.create({
+        name ,
+        email,
+        image
+    });
+    res.send(createdUser);
 })
 
 app.listen(3600);
